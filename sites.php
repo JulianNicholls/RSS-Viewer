f9176107bdf4a0f358c072dc0ce689b4cd997bdf
Julian Nicholls <juliannicholls29@gmail.com>
2015-08-20 19:23:41 +0100

    Add some comments.

--- sites.php
+++ sites.php
@@ -11,9 +11,9 @@ class Sites
 
     // Compare two site entries by name
 
-    static function cmp_sites( $a, $b )
+    static function cmp_sites($a, $b)
     {
-        return strcmp( $a['name'], $b['name'] );
+        return strcmp($a['name'], $b['name']);
     }
 
     // Attach to MongoDB and select sites collection from feeds DB.
@@ -25,40 +25,35 @@ class Sites
         $this->sites  = $mconn->feeds->sites;
     }
 
-
     // Insert a new feed
 
-    public function insert( $data )
+    public function insert($data)
     {
-        return $this->sites->insert( $data );
+        return $this->sites->insert($data);
     }
-
 
     // Remove a feed by its ID.
 
-    public function remove_by_id( $id )
+    public function remove_by_id($id)
     {
-        return $this->sites->remove( array( '_id' => new MongoId( $id ) ) );
+        return $this->sites->remove(array('_id' => new MongoId($id)));
     }
-
 
     // Remove a feed by its name.
 
-    public function remove_by_name( $name )
+    public function remove_by_name($name)
     {
-        return $this->sites->remove( array( 'name' => $name ) );
+        return $this->sites->remove(array('name' => $name));
     }
-
 
     // Update a feed by its ID.
 
-    public function update( $id, $data )
+    public function update($id, $data)
     {
-        return $this->sites->update( array( '_id' => new MongoId( $id ) ), $data );
+        return $this->sites->update(array('_id' => new MongoId($id)), $data);
     }
 
-
-    // Return all the feed sites, sorted by name
+    // Return all the feed site urls, sorted by name
 
     public function all()
     {
@@ -67,6 +62,8 @@ class Sites
         return $this->cursor_to_array($cursor);
     }
 
+    // Return the urls for sites that are part of the aggregated feed.
+
     public function aggregated_feed()
     {
         $cursor = $this->sites->find(array('aggregate' => 1));
@@ -74,33 +71,34 @@ class Sites
         return $this->cursor_to_array($cursor);
     }
 
+    // Collect and sort the urls from a MongoDB cursor.
+
     private function cursor_to_array($cursor)
     {
         $urls   = array();
 
-        foreach( $cursor as $cur )
+        foreach($cursor as $cur)
             $urls[] = $cur;
 
-        usort( $urls, array( "Sites", "cmp_sites" ) );
+        usort($urls, array("Sites", "cmp_sites"));
 
         return $urls;
     }
 
     // Return the data for a feed by its ID.
 
-    public function find_by_id( $id )
+    public function find_by_id($id)
     {
-        $cur = $this->sites->findOne( array( '_id' => new MongoId( $id ) ) );
+        $cur = $this->sites->findOne(array('_id' => new MongoId($id)));
 
         return $cur ? $cur : FALSE;
     }
 
-
     // Return the data for a feed by its name.
 
-    public function find_by_name( $name )
+    public function find_by_name($name)
     {
-        $cur = $this->sites->findOne( array( 'name' => $name ) );
+        $cur = $this->sites->findOne(array('name' => $name));
 
         return $cur ? $cur : FALSE;
     }
